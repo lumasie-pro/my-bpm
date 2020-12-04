0b69cae85cf526f04ed259fdf0a394a969b1faf6
liangliz <liangliz@esquel.com>
2020-12-04 16:06:20 +0800

    add some files

--- src/main.js
+++ src/main.js
@@ -1,7 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+import 'bpmn-js/dist/assets/diagram-js.css'
+import 'bpmn-js/dist/assets/bpmn-font/css/bpmn.css'
+import 'bpmn-js/dist/assets/bpmn-font/css/bpmn-codes.css'
+import 'bpmn-js/dist/assets/bpmn-font/css/bpmn-embedded.css'
+import './components/bpmn/assets/css/vue-bmpn.css'
+
+
 Vue.config.productionTip = false
+Vue.use(ElementUI)
 
 new Vue({
   render: h => h(App),
